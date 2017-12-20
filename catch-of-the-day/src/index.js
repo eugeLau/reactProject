b467885743cb5586cc2541c530bf29e2d25dc0f4
Eugenia Lau <eugenia.ch.lau@gmail.com>
2017-12-20 16:14:52 -0500

    finished Routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,20 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss} from 'react-router';
+import './css/style.css';
+import App from './components/app';
+import StorePicker from './components/StorePicker';
+
+const  Root = () => {
+    return (
+        <BrowserRouter>
+        <div>
+            <Match exactly pater="/" component={StorePicker} />
+            <Match pattern="/store/:storeId" component={App} />
+            <Miss component={notFound} />
+        </div>
+        </BrowserRouter>
+    )
+}
+render(<Root/>, document.querySelector('#main'));
